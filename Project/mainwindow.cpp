671bb4430f8977c422469af19e48feb706187b96
Ishika102 <58440964+Ishika102@users.noreply.github.com>
2020-04-17 18:20:45 +0530

    Update mainwindow.cpp

--- Project/mainwindow.cpp
+++ Project/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include<QFile>
+#include<QTextStream>
+#include<QMessageBox>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +15,26 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QFile file("C:/Users/K.m.C/Documents/Project/output.txt");
+
+    if(!file.open(QFile::ReadOnly | QFile::Text))
+    {
+            QMessageBox::warning(this,"title","file not open");
+    }
+    QTextStream in (&file);
+    while(!in.atEnd())
+    {
+        QString text = in.readLine();
+        QStringList fields = text.split( ";" );
+        ui->textEdit->setPlainText(fields[0]);
+        ui->textEdit_2->setPlainText(fields[1]);
+        ui->textEdit_3->setPlainText(fields[2]);
+        ui->textEdit_5->setPlainText(fields[3]);
+        ui->textEdit_4->setPlainText(fields[4]);
+    }
+
+    file.close();
+}
