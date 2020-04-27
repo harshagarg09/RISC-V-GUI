c40669def155103c8c33d5a7fbd6c17e4b3218d3
harshagarg09 <39459950+harshagarg09@users.noreply.github.com>
2020-04-28 00:03:59 +0530

    Update mainwindow.h

--- testProject/mainwindow.h
+++ testProject/mainwindow.h
@@ -32,30 +32,17 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    virtual void paintEvent(QPaintEvent *event);
+
 private slots:
     void on_pushButton_clicked();
-    void updateISB(InterStateBuffers &isb);
-    void updateAfterDecoder(InterStateBuffers &isb);
-    void updateIfStall(InterStateBuffers &isb);
-    void memory(InterStateBuffers &isb,MemoryAccess &memAccess ,MUX_Y &muxy);
-    void writeBack(InterStateBuffers &isb, RegUpdate &regUpdate, Registry_File &rFile);
-    void printOnTextEdit();
 
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_2_clicked();
+    void writeInInputFile();
+    void generateOutputFile();
 private:
     Ui::MainWindow *ui;
-    int count;
-    int i=0,j=0;
-    bool end=false;
-    InterStateBuffers isb;
-    Registry_File rFile;
-    Fetch fetch;
-    MUX_Y muxy;
-    Decode decode;
-    RegUpdate regUpdate;
-    MemoryAccess *memAccess;
-    ALU alu;
-    IAG iag;
-    QVector<QStringList>ins;
+
 };
 #endif // MAINWINDOW_H
