aad7cb8e0f0afffe9b7f37240ce9b5cb4d23dcd9
Ishika102 <58440964+Ishika102@users.noreply.github.com>
2020-04-28 15:46:57 +0530

    Update dialog.h

--- almostComplete/dialog.h
+++ almostComplete/dialog.h
@@ -1,8 +1,25 @@
 #ifndef DIALOG_H
 #define DIALOG_H
 #include"RegistryFile.h"
-
 #include <QDialog>
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string.h>
+#include<bitset>
+#include"ALU.h"
+#include"IAG.h"
+#include"Decode.h"
+#include"Fetch.h"
+#include"MUX_Y.h"
+#include"RegistryFile.h"
+#include"RegUpdate.h"
+#include"MemoryAccess.h"
+#include"InterStateBuffers.h"
+#include<string>
+#include<QFile>
+#include<QTextStream>
+#include <QMainWindow>
 #include <QPainter>
 namespace Ui {
 class Dialog;
@@ -19,12 +36,30 @@ public:
 
 private slots:
     void on_pushButton_clicked();
+    void memory(InterStateBuffers &isb,MemoryAccess &memAccess ,MUX_Y &muxy);
+    void writeBack(InterStateBuffers &isb, RegUpdate &regUpdate, Registry_File &rFile);
+    void printOnTextEdit();
+    void print(int i, InterStateBuffers &isb, Registry_File &rFile);
+    void printSummary(InterStateBuffers &isb);
 
 private:
     Ui::Dialog *ui;
+    QPaintEvent *event;
+    int count;
+    int i=0,j=0;
+    bool end=false;
+    InterStateBuffers isb;
     Registry_File rFile;
-    int i=0;
+    Fetch fetch;
+    MUX_Y muxy;
+    Decode decode;
+    RegUpdate regUpdate;
+    MemoryAccess *memAccess;
+    ALU alu;
+    IAG iag;
+    QVector<QStringList>ins;
 
 };
 
 #endif // DIALOG_H
+
